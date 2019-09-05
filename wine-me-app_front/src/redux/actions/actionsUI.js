45ce5d91c057a7590d4892664846eef790b9eb63
Denis-NV <denisnv@gmail.com>
2019-09-05 17:18:26 +0100

    Added initial loading logic. Connected to db dictionary.

--- wine-me-app_front/src/redux/actions/actionsUI.js
+++ wine-me-app_front/src/redux/actions/actionsUI.js
@@ -1,11 +1,30 @@
+import axios from "axios";
+
 export const SET_ERRORS = "SET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const SET_DEVICE_FLAGS = "SET_DEVICE_FLAGS";
+export const SET_INIT_DATA = "SET_INIT_DATA";
 export const SET_WINE_STYLE_FOCUS = "SET_WINE_STYLE_FOCUS";
 
-export const setTouchScreenFlag = (isTouchScreen, isSmarphone) => dispatch => {
+export const setTouchScreenFlag = (isTouchScreen, isSmartphone) => dispatch => {
   //
-  dispatch({ type: SET_DEVICE_FLAGS, payload: { isTouchScreen, isSmarphone } });
+  dispatch({
+    type: SET_DEVICE_FLAGS,
+    payload: { isTouchScreen, isSmartphone }
+  });
+};
+
+export const loadGlobalDictionary = lang => dispatch => {
+  //
+  axios
+    .get(`/dict/${lang}`)
+    .then(res => {
+      // console.log(res.data);
+      dispatch({ type: SET_INIT_DATA, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 export const setWineStyleFocus = index => dispatch => {
